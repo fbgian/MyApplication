f729fc90e33b2c05ed3a113782d9e9953b7985ab
Fabio <fgchaka@gmail.com>
2020-04-03 16:26:13 +0200

    Commit iniziale

---
+++ app/src/main/java/com/toilette/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.toilette.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
